c56b5463a737ed40da7a5712534ae8856aefe718
MarcCrusellas <71900639+MarcCrusellas@users.noreply.github.com>
2023-11-27 19:47:22 +0100

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,57 @@
+# Mostrar texto dúrate un tiempo determinado
+def show_text(text: str, time: int):
+    game.show_long_text(text, DialogLayout.BOTTOM)
+    pause(time)
+
+# Menú para seleccionar el tipo de conversión
+def menu(): 
+    opcio_usuari = game.ask_for_number("1. C° -> F° || 2. F° -> C°", 1)
+    if (opcio_usuari == 1 or opcio_usuari == 2):
+        return opcio_usuari
+    return None
+# Obtener el valor del cual se parte para hacer la conversión
+def get_number(opcio: int):
+    if (opcio ==1):
+        text = "Centígrads to convert"
+    elif (opcio ==1):
+        text = "Fahrenheit to convert"
+    else: 
+        text = "Don't look at me, this is an error"
+    val = game.ask_for_number(text)
+    return val
+
+# Función para hacer el cálculo de Centígrados a Fahrenheit
+def from_C_to_F(num: int):
+    return (num * (9/5)) + 32
+
+# Función para hacer el cálculo de Fahrenheit a Centígrados
+def from_F_to_C(num: int):
+    # eturn (num * (9/5)) + 32
+    return (num-32)*5/9
+
+# Gestionar cuál de las dos funciones es la que se llama
+def Calc(opcio: int, val: int):
+    if (opcio ==1):
+        toRet = from_C_to_F(val)
+    elif (opcio == 2):
+        toRet =  from_F_to_C(val)
+    return toRet
+
+# Función inicial para inicial el conversor
+def GameCF():
+    show_text("Convertidor de temperatura entre graus Centígrads i graus Fahrenheit.", 2)
+    option = menu()
+    if (option == None):
+        return
+    value = get_number(option)
+
+    asw = Calc(option, value)
+    if (option ==1):
+        show_text("From C° "+value+" Centígrads = " +Math.round_with_precision(asw, 2)+" Fahrenheit", 2)
+    else:
+        show_text("From F° "+value+" Fahrenheit = " +Math.round_with_precision(asw, 2)+" Centígrads", 2)
+
+
+forever(GameCF)
+
+
---
+++ main.ts
@@ -0,0 +1,71 @@
+//  Mostrar texto dúrate un tiempo determinado
+function show_text(text: string, time: number) {
+    game.showLongText(text, DialogLayout.Bottom)
+    pause(time)
+}
+
+//  Menú para seleccionar el tipo de conversión
+function menu(): number {
+    let opcio_usuari = game.askForNumber("1. C° -> F° || 2. F° -> C°", 1)
+    if (opcio_usuari == 1 || opcio_usuari == 2) {
+        return opcio_usuari
+    }
+    
+    return null
+}
+
+//  Obtener el valor del cual se parte para hacer la conversión
+function get_number(opcio: number): number {
+    let text: string;
+    if (opcio == 1) {
+        text = "Centígrads to convert"
+    } else if (opcio == 1) {
+        text = "Fahrenheit to convert"
+    } else {
+        text = "Don't look at me, this is an error"
+    }
+    
+    let val = game.askForNumber(text)
+    return val
+}
+
+//  Función para hacer el cálculo de Centígrados a Fahrenheit
+function from_C_to_F(num: number): number {
+    return num * (9 / 5) + 32
+}
+
+//  Función para hacer el cálculo de Fahrenheit a Centígrados
+function from_F_to_C(num: number): number {
+    //  eturn (num * (9/5)) + 32
+    return (num - 32) * 5 / 9
+}
+
+//  Gestionar cuál de las dos funciones es la que se llama
+function Calc(opcio: number, val: number): number {
+    let toRet: number;
+    if (opcio == 1) {
+        toRet = from_C_to_F(val)
+    } else if (opcio == 2) {
+        toRet = from_F_to_C(val)
+    }
+    
+    return toRet
+}
+
+//  Función inicial para inicial el conversor
+forever(function GameCF() {
+    show_text("Convertidor de temperatura entre graus Centígrads i graus Fahrenheit.", 2)
+    let option = menu()
+    if (option == null) {
+        return
+    }
+    
+    let value = get_number(option)
+    let asw = Calc(option, value)
+    if (option == 1) {
+        show_text("From C° " + value + " Centígrads = " + Math.roundWithPrecision(asw, 2) + " Fahrenheit", 2)
+    } else {
+        show_text("From F° " + value + " Fahrenheit = " + Math.roundWithPrecision(asw, 2) + " Centígrads", 2)
+    }
+    
+})
